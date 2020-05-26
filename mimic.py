02610278c5b0fb159cb23e6cc205b269c34b33dd
Jackson Nathaniel Detke <jndetke2150@gmail.com>
2020-05-26 15:09:49 -0400

    Initial commit of dev branch to DJJD2150's repo.

--- mimic.py
+++ mimic.py
@@ -19,7 +19,7 @@ You can try adding in line breaks around 70 columns so the output looks
 better.
 """
 
-__author__ = "???"
+__author__ = "DJJD2150, Jaspal Singh, Tiffany McLean"
 
 import random
 import sys
@@ -43,8 +43,26 @@ def create_mimic_dict(filename):
                 "who" : ["knows"]
             }
     """
-    # +++your code here+++
-    pass
+    # opens file and assigns it to a variable
+    with open(filename, "r") as f:
+        # creates a dictionary to store the file's words in as keys and values
+        mimic_dict = {}
+        # reads the file and separates it into lines
+        line = f.read().split()
+        # creates a variable for the starting word and makes it a string
+        start_word = ""
+        # loops through the lines to make words
+        for word in line:
+            # determines whether or not the previous word already exists 
+            # in "mimic_dict".  If it doesn't, it assigns the current word to the 
+            # previous word as a value.  If it does, it assigns the current word 
+            # to the priorly existing previous word as another value.
+            # Also makes the previous word the current word to keep the loop running.
+            if start_word not in mimic_dict:
+                mimic_dict[start_word] = [word]
+            else:
+                mimic_dict[start_word].append(word)
+    return mimic_dict
 
 
 def print_mimic(mimic_dict, start_word):
@@ -55,8 +73,19 @@ def print_mimic(mimic_dict, start_word):
         - Randomly select a new word from the next-list
         - Repeat this process 200 times
     """
-    # +++your code here+++
-    pass
+    # starts a new string variable that will convert mimic_dict from a dictionary to a string
+    next_list = ""
+    # creates a new list to loop through that is 200 words long
+    for _ in list(range(200)):
+        # assigns the carried over word values/ keys from the prior function to the "random_dict"
+        # variable in this function
+        random_dict = mimic_dict[start_word]
+        # chooses 200 words at random
+        new_word = random.choice(random_dict)
+        # concatenates the words into the "next_list" string
+        next_list += " " + new_word
+    # prints the string
+    print(next_list)
 
 
 # Provided main(), calls mimic_dict() and print_mimic()
